e618c8d1cfd49df361045191a7c52f3c04c055aa
Thibault Lafargue <thibault.gplus@gmail.com>
2023-02-21 14:48:59 +0100

    Solve Rails wikinimous challenge

---
+++ cubes.rb
@@ -0,0 +1,6 @@
+
+a1 = [3, 2, 1, 2]
+a2 = [1, 4, 5, 3, 5 ]
+
+p a1.sort
+p a2.sort.reverse
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+10.times do
+  Article.create(title: Faker::Lorem.sentence(word_count: 5), content: Faker::Lorem.paragraph(sentence_count: 5))
+end
